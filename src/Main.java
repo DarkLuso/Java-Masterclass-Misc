ff39ebc2b170411464459203cf7b0bf0a7bf907d
Miguel Dummer <darckluso@gmail.com>
2020-07-22 04:22:53 -0300

    Interfaces Challenge

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -37,10 +40,78 @@ public class Main {
 //        myPhone.addContact("Johnny","51 999223478");
 //        myPhone.addContact("CJ","51 997788345");
 //        myPhone.addContact("Johnny","51 999223478");
-        MobilePhone myPhone = new MobilePhone();
-        myPhone.startPhone();
+//        MobilePhone myPhone = new MobilePhone();
+//        myPhone.startPhone();
+//        LinkedList<String> myLinkedList = new LinkedList<>();
+//        myLinkedList.add("1");
+//        myLinkedList.add("3");
+//        myLinkedList.add("4");
+//        myLinkedList.add("5");
+//        myLinkedList.add("6");
+//
+//
+//        Iterator<String> myIterator = myLinkedList.iterator();
+//
+//        while (myIterator.hasNext()){
+//            System.out.println(myIterator.next());
+//        }
+//
+//        myLinkedList.add(1,"2");
+//
+//        for (String s : myLinkedList){
+//            System.out.println(s);
+//
+//
+//        }
+        Player jack = new Player("Jack",13,12);
+        saveObject(jack);
+        System.out.println(jack.toString());
+        loadObject(jack);
+        saveObject(jack);
 
 
-    }
+   }
+
+   public static ArrayList<String> readValues (){
+
+        ArrayList<String> values = new ArrayList<>();
+       Scanner scanner = new Scanner(System.in);
+       boolean quit = false;
+       int index = 0;
+       System.out.println("Chose \n" +
+               "Press 1 to enter a String \n" +
+               "Press 2 to quit");
+       while (!quit){
+           System.out.println("Chose a Option ");
+           int choice = scanner.nextInt();
+           scanner.nextLine();
+           switch (choice){
+               case 1:
+                   System.out.println("Enter a string: ");
+                   String stringInput = scanner.nextLine();
+                   values.add(index, stringInput);
+                   index++;
+                   break;
+               case 2:
+                   quit = true;
+                   break;
+           }
+       }
+
+       return values;
+
+   }
+
+   public static void saveObject (ISaveable objectToSave){
+        for (int i = 0; i<objectToSave.write().size();i++){
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+   }
+
+   public static void loadObject (ISaveable objectToLoad){
+        ArrayList<String> value = readValues();
+        objectToLoad.read(value);
+
+   }
 
 }
